fe5704860d54225c33b018a8c2789c2a87e2cbc7
xiangyuwang <xiangyuwang.chn@qq.com>
2023-03-28 11:53:20 +0800

    update

--- python/briefRotTest.py
+++ python/briefRotTest.py
@@ -1,18 +1,28 @@
 import numpy as np
 import cv2
 from matchPics import matchPics
+import scipy
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
+# Q3.5
+# Read the image and convert to grayscale, if necessary
+cv_cover = cv2.imread('../data/cv_cover.jpg')
+# cv_desk = cv2.imread('../data/cv_desk.png')
 
-#Q3.5
-#Read the image and convert to grayscale, if necessary
+histogram_list = []
+for i in tqdm(range(36)):
+    # Rotate Image
+    rot_desk = scipy.ndimage.rotate(cv_cover, angle=i * 10)
+    # cv2.imshow('ori', cv_cover)
+    # cv2.imshow('rot', rot_desk)
+    # cv2.waitKey(0)
+    # Compute features, descriptors and Match features
+    matches, locs1, locs2 = matchPics(cv_cover, rot_desk)
+    # Update histogram
+    histogram_list.append(len(matches))
 
-for i in range(36):
-	#Rotate Image
-	
-	#Compute features, descriptors and Match features
-
-	#Update histogram
-
-
-#Display histogram
-
+# Display histogram
+x = list(range(0, 360, 10))
+plt.bar(x, histogram_list, width=4)
+plt.show()
--- python/matchPics.py
+++ python/matchPics.py
@@ -5,20 +5,23 @@ from helper import briefMatch
 from helper import computeBrief
 from helper import corner_detection
 
+
 def matchPics(I1, I2):
-	#I1, I2 : Images to match
-	
+    # I1, I2 : Images to match
+    image1 = I1
+    image2 = I2
 
-	#Convert Images to GrayScale
-	
-	
-	#Detect Features in Both Images
-	
-	
-	#Obtain descriptors for the computed feature locations
-	
-
-	#Match features using the descriptors
-	
-
-	return matches, locs1, locs2
+    # Convert Images to GrayScale
+    if len(image1.shape) >= 3:
+        image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+    if len(image2.shape) >= 3:
+        image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    # Detect Features in Both Images
+    locs1 = corner_detection(image1)
+    locs2 = corner_detection(image2)
+    # Obtain descriptors for the computed feature locations
+    desc1, locs1 = computeBrief(image1, locs1)
+    desc2, locs2 = computeBrief(image2, locs2)
+    # Match features using the descriptors
+    matches = briefMatch(desc1, desc2)
+    return matches, locs1, locs2
