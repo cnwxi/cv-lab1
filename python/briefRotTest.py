4d47574033b486766602dcb5e734665cb5e7351d
xiangyuwang <xiangyuwang.chn@qq.com>
2023-03-28 15:55:52 +0800

    update

--- python/briefRotTest.py
+++ python/briefRotTest.py
@@ -1,9 +1,10 @@
-import numpy as np
 import cv2
-from matchPics import matchPics
-import scipy
 import matplotlib.pyplot as plt
+import scipy
 from tqdm import tqdm
+
+from helper import plotMatches
+from matchPics import matchPics
 
 # Q3.5
 # Read the image and convert to grayscale, if necessary
@@ -13,12 +14,14 @@ cv_cover = cv2.imread('../data/cv_cover.jpg')
 histogram_list = []
 for i in tqdm(range(36)):
     # Rotate Image
-    rot_desk = scipy.ndimage.rotate(cv_cover, angle=i * 10)
+    rot_cover = scipy.ndimage.rotate(cv_cover, angle=i * 10)
     # cv2.imshow('ori', cv_cover)
     # cv2.imshow('rot', rot_desk)
     # cv2.waitKey(0)
     # Compute features, descriptors and Match features
-    matches, locs1, locs2 = matchPics(cv_cover, rot_desk)
+    matches, locs1, locs2 = matchPics(cv_cover, rot_cover)
+    if i % 12 == 0:
+        plotMatches(cv_cover, rot_cover, matches, locs1, locs2)
     # Update histogram
     histogram_list.append(len(matches))
 
