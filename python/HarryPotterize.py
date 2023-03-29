225a486eb78dc5c9d5223efa8feb4dd321211994
xiangyuwang <xiangyuwang.chn@qq.com>
2023-03-29 11:17:35 +0800

    update

--- python/HarryPotterize.py
+++ python/HarryPotterize.py
@@ -1,9 +1,32 @@
-import numpy as np
 import cv2
-import skimage.io 
-import skimage.color
-#Import necessary functions
+from matplotlib import pyplot as plt
+
+# Import necessary functions
+from matchPics import matchPics
+from planarH import compositeH
+from planarH import computeH_ransac
 
 
+# Write script for Q3.9
+def Q3_9():
+    cv_cover = cv2.imread('../data/cv_cover.jpg')
+    cv_desk = cv2.imread('../data/cv_desk.png')
+    hp_cover = cv2.imread('../data/hp_cover.jpg')
+    matches, locs1, locs2 = matchPics(cv_desk, cv_cover)
+    locs1 = locs1[matches[:, 0]]
+    locs1 = locs1[:, [1, 0]]
+    locs2 = locs2[matches[:, 1]]
+    locs2 = locs2[:, [1, 0]]
+    bestH2to1, inliers = computeH_ransac(locs1, locs2)
+    hp_cover = cv2.resize(hp_cover, ((cv_cover.shape[1]), cv_cover.shape[0]))
+    comp_img = compositeH(bestH2to1, hp_cover, cv_desk)
+    list = [cv_cover, cv_desk, hp_cover, comp_img]
+    plt.figure()
+    for i, item in enumerate(list):
+        plt.subplot(1, 4, i + 1)
+        plt.imshow(cv2.cvtColor(item, cv2.COLOR_BGR2RGB))
+    plt.show()
 
-#Write script for Q3.9
+
+if __name__ == '__main__':
+    Q3_9()
--- python/planarH.py
+++ python/planarH.py
@@ -1,73 +1,105 @@
-import numpy as np
 import cv2
+import numpy as np
 
 
 def computeH(x1, x2):
-	#Q3.6
-	#Compute the homography between two sets of points
-
-
-
-
-	return H2to1
+    # Q3.6
+    # Compute the homography between two sets of points
+    tmp, _ = np.shape(x1)
+    tmp_list = []
+    tmp_array = None
+    for i in range(tmp):
+        tmp_list.append([-x2[i, 0], -x2[i, 1], -1, 0, 0, 0, x1[i, 0] * x2[i, 0], x1[i, 0] * x2[i, 1], x1[i, 0]])
+        tmp_list.append([0, 0, 0, -x2[i, 0], -x2[i, 1], -1, x1[i, 1] * x2[i, 0], x1[i, 1] * x2[i, 1], x1[i, 1]])
+        tmp_array = np.array(tmp_list)
+    _, _, v = np.linalg.svd(tmp_array)
+    H2to1 = np.reshape(v[-1, :] / v[-1, -1], (3, 3))
+    return H2to1
 
 
 def computeH_norm(x1, x2):
-	#Q3.7
-	#Compute the centroid of the points
-
-
-	#Shift the origin of the points to the centroid
-
-
-	#Normalize the points so that the largest distance from the origin is equal to sqrt(2)
-
-
-	#Similarity transform 1
-
-
-	#Similarity transform 2
-
-
-	#Compute homography
-
-
-	#Denormalization
-	
-
-	return H2to1
-
-
+    # Q3.7
+    # Compute the centroid of the points
+    length = x1.shape[0]
+    x1_mean = np.mean(x1, axis=0)
+    x2_mean = np.mean(x2, axis=0)
+    # Shift the origin of the points to the centroid
+    x1_shift = x1 - x1_mean
+    x2_shift = x2 - x2_mean
+    # Normalize the points so that the largest distance from the origin is equal to sqrt(2)
+    x1_norm = np.max(np.linalg.norm(x1_shift, axis=1))
+    x2_norm = np.max(np.linalg.norm(x2_shift, axis=1))
+    x1_sqrt = np.sqrt(2) / x1_norm
+    x2_sqrt = np.sqrt(2) / x1_norm
+    x1_shift = x1_shift * x1_sqrt
+    x2_shift = x2_shift * x2_sqrt
+    # Similarity transform 1
+    t1 = np.asarray([[x1_sqrt, 0, -x1_sqrt * x1_mean[0]],
+                     [0, x1_sqrt, -x1_sqrt * x1_mean[1]],
+                     [0, 0, 1]])
+    x1 = np.hstack((x1, np.ones(length).reshape(-1, 1)))
+    # Similarity transform 2
+    t2 = np.asarray([[x2_sqrt, 0, -x2_sqrt * x2_mean[0]],
+                     [0, x2_sqrt, -x2_sqrt * x2_mean[1]],
+                     [0, 0, 1]])
+    x2 = np.hstack((x2, np.ones(length).reshape(-1, 1)))
+    # Compute homography
+    H2to1 = computeH(x1_shift, x2_shift)
+    # Denormalization
+    H2to1 = np.matmul(np.linalg.inv(t1), H2to1)
+    H2to1 = np.matmul(H2to1, t2)
+    return H2to1
 
 
 def computeH_ransac(x1, x2):
-	#Q3.8
-	#Compute the best fitting homography given a list of matching points
-
-
-
-	return bestH2to1, inliers
-
+    # Q3.8
+    # Compute the best fitting homography given a list of matching points
+    inliers = []
+    iterations = 1000
+    inlier_tmp = 0
+    bestH2to1 = None
+    for i in range(iterations):
+        idx = np.arange(0, len(x1))
+        np.random.shuffle(idx)
+        ptsel = idx[0:4]
+        H2to1 = computeH_norm(x1[ptsel], x2[ptsel])
+        x2_ = np.hstack((x2, np.ones(len(x2)).reshape(-1, 1)))
+        x1_ = np.matmul(H2to1, x2_.T)[0:2, :]
+        dist = np.linalg.norm(x1.T - x1_, axis=0)
+        inlier = (dist < 2).astype(int)
+        inlier_cont = np.sum(inlier)
+        if inlier_cont > inlier_tmp:
+            inliers = inlier
+            bestH2to1 = H2to1
+            inlier_tmp = inlier_cont
+    if len(inliers) > 1:
+        index = [i for i, j in enumerate(inliers) if j == 1]
+        if len(index) >= 4:
+            bestH2to1 = computeH_norm(x1[index], x2[index])
+    else:
+        bestH2to1 = np.eye(3)
+    return bestH2to1, inliers
 
 
 def compositeH(H2to1, template, img):
-	
-	#Create a composite image after warping the template image on top
-	#of the image using the homography
+    # Create a composite image after warping the template image on top
+    # of the image using the homography
 
-	#Note that the homography we compute is from the image to the template;
-	#x_template = H2to1*x_photo
-	#For warping the template to the image, we need to invert it.
-	
+    # Note that the homography we compute is from the image to the template;
+    # x_template = H2to1*x_photo
+    # For warping the template to the image, we need to invert it.
 
-	#Create mask of same size as template
-
-	#Warp mask by appropriate homography
-
-	#Warp template by appropriate homography
-
-	#Use mask to combine the warped template and the image
-	
-	return composite_img
-
-
+    # Create mask of same size as template
+    mask_temp = np.ones((template.shape[0], template.shape[1]), dtype=np.uint8)
+    mask_img = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
+    # Warp mask by appropriate homography
+    composite_img = cv2.warpPerspective(template, H2to1, (img.shape[1], img.shape[0]))
+    # Warp template by appropriate homography
+    mask_temp_warp = cv2.warpPerspective(mask_temp, H2to1, (mask_img.shape[1], mask_img.shape[0]))
+    # Use mask to combine the warped template and the image
+    warp_not = cv2.bitwise_not(mask_temp_warp) // 255
+    warp_not = np.stack([warp_not, warp_not, warp_not])
+    warp_not = np.transpose(warp_not, (1, 2, 0))
+    background = img * warp_not
+    composite_img = background + composite_img
+    return composite_img
